9744bf9a95a5574e262e50723eafccbf9b04ccd5
WakkaTrout <36018858+WakkaTrout@users.noreply.github.com>
2026-04-10 09:23:21 -0700

    Update sd_card_device.rs

--- src/sd_card_device.rs
+++ src/sd_card_device.rs
@@ -1,6 +1,29 @@
 // Initialize, Read, and Write Utilities for an SD Card
 
-#[inline(always)]
-pub fn sd_card_read(address : u64, bytes_to_read: u64, buffer:&mut [u8]) {
+pub fn sd_card_initialize(address : u64) {
+    //TODO What needs to be done? Is it different between warm and cold boots?
+}
+
+pub fn sd_card_sync_read(address : u64, bytes_to_read: u64, buffer:&mut [u8]) -> () {
+    //TODO How do we talk to the SD card from an MPSoC?
+}
+
+pub fn sd_card_async_read(address : u64, bytes_to_read: u64, buffer:&mut [u8]) -> () {
+    //TODO How do we talk to the SD card from an MPSoC?
+}
+
+pub fn sd_card_async_read_complete() -> bool {
+    //TODO How do we talk to the SD card from an MPSoC?
+}
+
+pub fn sd_card_sync_write(address : u64, bytes_to_read: u64, buffer:&mut [u8]) -> () {
+    //TODO How do we talk to the SD card from an MPSoC?
+}
+
+pub fn sd_card_async_write(address : u64, bytes_to_read: u64, buffer:&mut [u8]) -> () {
+    //TODO How do we talk to the SD card from an MPSoC?
+}
+
+pub fn sd_card_async_write_complete() -> bool {
     //TODO How do we talk to the SD card from an MPSoC?
 }
