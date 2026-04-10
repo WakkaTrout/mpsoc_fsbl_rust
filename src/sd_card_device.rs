0d66e85e0e4c75332ae0d39f12888269c878c662
WakkaTrout <36018858+WakkaTrout@users.noreply.github.com>
2026-04-09 17:38:58 -0700

    Add file for SD card functionality in rust

---
+++ src/sd_card_device.rs
@@ -0,0 +1,6 @@
+// Initialize, Read, and Write Utilities for an SD Card
+
+#[inline(always)]
+pub fn sd_card_read(address : u64, bytes_to_read: u64, buffer:&mut [u8]) {
+    //TODO How do we talk to the SD card from an MPSoC?
+}
