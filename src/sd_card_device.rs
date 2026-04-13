884bba6dcacf238401fd7dfb2a74c1a4dbb8b7cd
WakkaTrout <36018858+WakkaTrout@users.noreply.github.com>
2026-04-13 12:07:03 -0700

    Began work on SD card initiailization

--- src/sd_card_device.rs
+++ src/sd_card_device.rs
@@ -1,9 +1,25 @@
+use crate::sdio_constants::*;
+
 // Initialize, Read, and Write Utilities for an SD Card
 
-pub fn sd_card_initialize(address : u64) {
-    //TODO What needs to be done? Is it different between warm and cold boots?
+pub fn sd_card_initialize(sd_card_id : SDCardId) {
+    // The following are the steps from the TRM
+
+
+    // Setup the SD Card Controller on the MPSoC
+    // TODO: Are we sure we should do this everytime we boot? Should there be cases we do not do this for warm boots?
+    //       Is there ever a case we want to do a partial reset?
+    sdio_software_reset_all(sd_card_id);
+    // TODO timeout
+    // TODO: How long does this take? Should we go do something else while this is resetting and then comeback?
+    while !sdio_all_in_reset(sd_card_id) {}
+
+
+
+    // Initialize the card (if inserted)
 }
 
+/*
 pub fn sd_card_sync_read(address : u64, bytes_to_read: u64, buffer:&mut [u8]) -> () {
     //TODO How do we talk to the SD card from an MPSoC?
 }
@@ -29,5 +45,6 @@ pub fn sd_card_async_write_complete() -> bool {
 }
 
 pub fn sd_check_fat_partition() {
-    // TODO We plan to support FAT partitions. Maybe we want to be able to detect and see if it is something other than FAT too?
+    // TODO
 }
+    */
